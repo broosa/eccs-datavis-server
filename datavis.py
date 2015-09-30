470ea244a68e593812f6235e07e809b8a6c5e61b
Byron Roosa <violinuxer@gmail.com>
2015-09-30 15:00:19 -0400

    Added request handler skeletons

--- datavis.py
+++ datavis.py
@@ -14,10 +14,23 @@ class RegexConverter(BaseConverter):
 
 app.url_map.converters['regex'] = RegexConverter
 
-@app.route('/api/<regex("[a-zA-Z0-9]+"):command>/<path:args>/')
-def req_api(command, args):
-    return render_template("base.html", command=command)
-    #return app.root_path
+
+#@app.route('/api/<regex("[a-zA-Z0-9]+"):command>/<path:args>/')
+#def req_api(command, args):
+#    return render_template("base.html", command=command)
+#    #return app.root_path
+
+@app.route('/api/datasets/')
+def api_list_datasets():
+	pass
+
+@app.route('/api/<regex("[a-zA-Z0-9]+"):dataset_name>/columns/')
+def api_list_variables(dataset_name):
+	pass
+
+@app.route('/api/<regex("[a-zA-Z0-9]+"):data_set_name>/<regex("[a-zA-Z0-9]+"):column_name>')
+def api_get_column(dataset_name, column_name):
+	pass
 
 #Start the testing server
 if __name__ == "__main__":
@@ -26,9 +39,8 @@ if __name__ == "__main__":
     
     app.logger.info("Loading auth info...")
     with open("auth.txt", "r") as auth_file:
-        host, db, username, password = [line for line in auth_file]
-    
+        host, db, username, password = [line.strip() for line in auth_file]
     app.logger.info("Connecting to postgres...")
-    db_conn = psycopg.connect(host=host, database=db, user=username, password=password)
+    db_conn = psycopg2.connect(host=host, database=db, user=username, password=password)
 
     app.run(debug=True)
