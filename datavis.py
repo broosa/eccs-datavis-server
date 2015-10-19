76d76a78920d7c1e5868cb1dc3e98300d15867fb
Byron Roosa <violinuxer@gmail.com>
2015-10-19 16:22:31 -0400

    Merged backend and frontend, started javascript code

--- datavis.py
+++ datavis.py
@@ -4,7 +4,7 @@ import flask
 
 from psycopg2.extensions import AsIs
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from werkzeug.routing import BaseConverter
 
 db_conn = None;
@@ -21,44 +21,47 @@ app.url_map.converters['regex'] = RegexConverter
 #This function should eventually be replaced to avoid getting
 #more data than we actually need
 def psql_query(query, args=None):
-	cursor = db_conn.cursor()
-	if args is not None:
-		cursor.execute(query, args)
-	else:
-		curser.execute(query)
-	return cursor.fetchall()
+    cursor = db_conn.cursor()
+    if args is not None:
+        cursor.execute(query, args)
+    else:
+        cursor.execute(query)
+    return cursor.fetchall()
 
 #@app.route('/api/<regex("[a-zA-Z0-9]+"):command>/<path:args>/')
 #def req_api(command, args):
 #    return render_template("base.html", command=command)
 #    #return app.root_path
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 @app.route('/api/datasets/')
 def api_list_datasets():
-	datasets = psql_query("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname='public';")
-	return flask.jsonify(datasets=[item[0] for item in datasets])
+    datasets = psql_query("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname='public';")
+    return flask.jsonify(datasets=[item[0] for item in datasets])
 
-@app.route('/api/<regex("[a-zA-Z0-9_]+"):dataset_name>/columns/')
-def api_list_columns(dataset_name):
-	columns = psql_query("SELECT column_name FROM information_schema.columns WHERE table_name=%s;", (dataset_name,))
-	#We don't want to return a list of lists in json; we only want the elements in each list. Hence, we unpack
-	#Each row so that each is a single litem in the JSON array
-	return flask.jsonify(dataset=dataset_name, columns=[col_name[0] for col_name in columns])
+@app.route('/api/<regex("[a-zA-Z0-9_]+"):dataset_name>/dates/')
+def api_list_dates(dataset_name):
+    dates = psql_query("SELECT DISTINCT ON (date(tstamp)) to_char(tstamp, 'YYYY-MM-DD') FROM %s;", (AsIs(dataset_name),))
+    #We don't want to return a list of lists in json; we only want the elements in each list. Hence, we unpack
+    #Each row so that each is a single litem in the JSON array
+    return flask.jsonify(dataset=dataset_name, dates=[date[0] for date in dates])
 
 @app.route('/api/<regex("[a-zA-Z0-9_]+"):dataset_name>/<regex("[a-zA-Z0-9_+]+"):column_names>/')
 def api_get_column(dataset_name, column_names):
-	data = None
+    data = None
 
-	columns = str(", ".join(column_names.split("+")))
-	if request.args.get("count"):
+    columns = str(", ".join(column_names.split("+")))
+    if request.args.get("count"):
 
-		record_count = int(request.args.get("count"))
-		data = psql_query("SELECT %s FROM %s LIMIT %s;", (AsIs(columns), AsIs(dataset_name), record_count))		
+        record_count = int(request.args.get("count"))
+        data = psql_query("SELECT %s FROM %s LIMIT %s;", (AsIs(columns), AsIs(dataset_name), record_count))		
 
-	else:
-		data = psql_query("SELECT %s FROM %s;", (AsIs(columns), AsIs(dataset_name)))
+    else:
+        data = psql_query("SELECT %s FROM %s;", (AsIs(columns), AsIs(dataset_name)))
 	
-	return flask.jsonify(dataset=dataset_name, data=data)
+    return flask.jsonify(dataset=dataset_name, data=data)
 
 #Start the testing server
 if __name__ == "__main__":
@@ -71,4 +74,4 @@ if __name__ == "__main__":
     app.logger.info("Connecting to postgres...")
     db_conn = psycopg2.connect(host=host, database=db, user=username, password=password)
 
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
---
+++ static/js/datavis.js
@@ -0,0 +1,56 @@
+var dataMap;
+
+var mapInit = function() {
+	
+	var mapProps = {
+		center: new google.maps.LatLng(68.000, 18.000),
+		zoom: 4,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+	dataMap = new google.maps.Map(document.getElementById("map-viewport"), mapProps);
+};
+
+var dropdownInit = function() {
+    $.ajax({
+        url: "api/datasets/",
+        success: function(data) {
+            console.log("Populating dropdowns...");
+            $.each(data.datasets, function(index, value) {
+                option = $("<option></option>").attr("value", value).text(value);
+                $("#dropdown-dataset").append(option);
+                $("#dropdown-dataset").trigger("chosen:updated", true);
+            });
+        }
+    });
+};
+
+var onDatasetChanged = function() {
+    console.log("Handling dropdown change event...");
+    chosenElement = $("#dropdown-dataset").chosen()
+    if (chosenElement.val() != "") {
+        $("#dropdown-date").removeAttr("disabled");
+    }
+};
+
+var onDateChanged = function() {
+
+};
+
+var onPlaceChanged = function() {
+
+};
+
+var onSampleTypeChanged = function() {
+
+};
+
+$(document).ready(function () {
+	console.log("Setting up comboboxes");
+	$(".chosen-select").chosen({disable_search_threshold: 10});
+    
+    dropdownInit();
+    $("#dropdown-dataset").chosen().change(onDatasetChanged);
+    $("#dropdown-date").chosen().change(onDateChanged);
+    $("#dropdown-place").chosen().change(onPlaceChanged);
+    $("#dropdown-sample-type").chosen().change(onSampleTypeChanged);
+});
