81c23ce73082442e2f0b74b3b24bcb437bb95d52
Byron Roosa <violinuxer@gmail.com>
2015-10-22 11:28:03 -0400

    Finished javascript code for dropdown population

--- datavis.py
+++ datavis.py
@@ -56,7 +56,7 @@ def api_list_places(dataset_name, date_string):
 @app.route('/api/<regex("[a-zA-Z0-9_]+"):dataset_name>/<regex("\d{4}-\d{2}-\d{2}"):date_string>/<regex("[a-zA-Z0-9_]+"):place_name>/sample-types/')
 def api_list_sample_types(dataset_name, date_string, place_name):
     s_types = psql_query("SELECT DISTINCT ON (sensor) sensor FROM %s WHERE date(tstamp) = (DATE %s) AND site=%s", (AsIs(dataset_name), date_string, place_name))
-    return flask.jsonify(dataset=dataset_name, date=date_string, place=place_string, sample_type=[s_type[0] for s_type in s_types])
+    return flask.jsonify(dataset=dataset_name, date=date_string, place=place_name, sampleTypes=[s_type[0] for s_type in s_types])
       
 @app.route('/api/<regex("[a-zA-Z0-9_]+"):dataset_name>/<regex("\d{4}-\d{2}-\d{2}"):date_string>/<regex("[a-zA-Z0-9_]+"):place_name>/<regex("[a-zA-Z0-9_]+"):sample_type>/')
 def api_list_data(dataset_name, date_string, place_name, sample_type):
--- static/js/datavis.js
+++ static/js/datavis.js
@@ -10,6 +10,22 @@ var mapInit = function() {
 	dataMap = new google.maps.Map(document.getElementById("map-viewport"), mapProps);
 };
 
+var resetDropdown = function(element) {
+	element.val(element.children("option:first").val());
+	element.children("option").not("[value='']").remove();
+	element.trigger("chosen:updated", true);
+}
+
+var enableDropdown = function(element) {
+	element.prop("disabled", false);
+	element.trigger("chosen:udpated", true);
+};
+
+var disableDropdown = function(element) {
+	element.prop("disabled", "disabled");
+	element.trigger("chosen:udpated", true);
+};
+
 var populateDropdown = function(element, data) {
     $.each(data, function(index, value) {
         option = $("<option></option>").attr("value", value).text(value);
@@ -23,7 +39,7 @@ var dropdownInit = function() {
         url: "api/datasets/",
         success: function(data) {
             console.log("Populating dropdowns...");
-            populateDropdown($("#dropdown-dataset"), data);
+            populateDropdown($("#dropdown-dataset"), data.datasets);
         }
     });
 };
@@ -32,27 +48,79 @@ var onDatasetChanged = function() {
     console.log("Handling dropdown change event...");
     chosenElement = $("#dropdown-dataset").chosen()
     if (chosenElement.val() != "") {
-        $("#dropdown-date").removeAttr("disabled");
-        $("#dropdown-date").trigger("chosen:updated", true);
         
         datasetName = chosenElement.val();
-        ajaxURL = "api/" + datasetName + "/dates/";
+        ajaxURL = ["api", datasetName, "dates/"].join("/");
+
         $.ajax({
             url: ajaxURL,
             success: function(data) {
                 console.log("Populating date dropdown");
+
+		        enableDropdown($("#dropdown-date"));
+	
+				resetDropdown($("#dropdown-date"));
+				resetDropdown($("#dropdown-place"));
+				resetDropdown($("#dropdown-sample-type"));
+	
+				disableDropdown($("#dropdown-place"));
+				disableDropdown($("#dropdown-sample-type"));
+
                 populateDropdown($("#dropdown-date"), data.dates);
             }
         });
-    }
+    } 
 };
 
 var onDateChanged = function() {
+	chosenElement = $("#dropdown-date").chosen();
+	if (chosenElement.val() != "") {
+		console.log("foobar");
+		
+		datasetName = $("#dropdown-dataset").chosen().val();
+		date = chosenElement.val();
+		
+		ajaxURL = ["api", datasetName, date, "places/"].join("/");
+		$.ajax({
+			url: ajaxURL,
+			success: function(data) {
+				console.log("Populating place dropdown");
 
+				enableDropdown($("#dropdown-place"));
+				
+				resetDropdown($("#dropdown-place"));
+				resetDropdown($("#dropdown-sample-type"));
+			
+				disableDropdown($("#dropdown-sample-type"));
+				populateDropdown($("#dropdown-place"), data.places);
+				
+			}
+		});
+	}
 };
 
 var onPlaceChanged = function() {
+	
+	chosenElement = $("#dropdown-place").chosen();
+	if (chosenElement.val() != "") {
+		datasetName = $("#dropdown-dataset").chosen().val();
+		date = $("#dropdown-date").chosen().val();
+		place = chosenElement.val();
+		
+		ajaxURL = ["api", datasetName, date, place, "sample-types/"].join("/");
 
+		$.ajax({
+			url: ajaxURL,
+			success: function(data) {
+				console.log("Populating place dropdown");
+
+				enableDropdown($("#dropdown-sample-type"));
+				resetDropdown($("#dropdown-sample-type"));
+			
+				populateDropdown($("#dropdown-sample-type"), data.sampleTypes);
+			}
+		});
+	}
 };
 
 var onSampleTypeChanged = function() {
